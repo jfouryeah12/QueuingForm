b12d00a0aa5aa89372a876aaba30b54dc7c11e59
Echo <40499446+jfouryeah12@users.noreply.github.com>
2021-12-16 01:14:03 +0800

    Add files via upload

---
+++ QueuingSystem/QueuingSystem/CashierWindowQueueForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace QueuingSystem
+{
+    partial class CashierWindowQueue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCashierQueue = new System.Windows.Forms.ListView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listCashierQueue
+            // 
+            this.listCashierQueue.Location = new System.Drawing.Point(154, 12);
+            this.listCashierQueue.Name = "listCashierQueue";
+            this.listCashierQueue.Size = new System.Drawing.Size(254, 412);
+            this.listCashierQueue.TabIndex = 0;
+            this.listCashierQueue.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Alef", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRefresh.Location = new System.Drawing.Point(10, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(138, 42);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Alef", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNext.Location = new System.Drawing.Point(10, 60);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(138, 42);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // CashierWindowQueue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 433);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listCashierQueue);
+            this.Name = "CashierWindowQueue";
+            this.Text = "CashierWindowQueueForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListView listCashierQueue;
+        private Button btnRefresh;
+        private Button btnNext;
+    }
+}
---
+++ QueuingSystem/QueuingSystem/QueuingForm.cs
@@ -0,0 +1,19 @@
+namespace QueuingSystem
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        //Modify
+        private CashierClass cashier = new CashierClass();
+        public Form()
+        {
+            InitializeComponent();
+        }
+        //Start
+        private void btnCashier_Click(object sender, EventArgs e)
+        {
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+        }
+    }
+}
